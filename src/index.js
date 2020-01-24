c25b2dc063afea031e7825ac63812c1d85b00c8f
sj-log <sjisyours@gmail.comDD>
2020-01-24 21:27:13 +0800

    react-ga trying to install

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(`UA-116676814-5`);
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
