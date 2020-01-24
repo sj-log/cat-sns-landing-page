4d6845ad20d13e56583138d6b9443e9d5d14d15c
sj-log <sjisyours@gmail.comDD>
2020-01-24 20:10:22 +0800

    seo optimization with open graph set / favicon

--- src/App.js
+++ src/App.js
@@ -52,60 +52,116 @@ function App() {
   return (
     <>
       <header>
-        <img
-          className="cat-sns-main-img"
-          alt="cat-sns-logo-hug-seven"
-          src="https://user-images.githubusercontent.com/35059428/73043231-39e12500-3e9f-11ea-8fd2-ba1ed33a08c1.jpg"
-        />
+        <Popover
+          placement="right"
+          content={
+	   <h1>😻️</h1>
+          }>
+          <img
+            className="cat-sns-main-img"
+            alt="cat-sns-logo-hug-seven"
+            src="https://user-images.githubusercontent.com/35059428/73043231-39e12500-3e9f-11ea-8fd2-ba1ed33a08c1.jpg"
+          />
+        </Popover>
         <h1>홀썸이네</h1>
-        <p>@hug.seven</p>
+        <p>세상에 버림받았던 아이들과의 묘연</p>
       </header>
       <Layout id="lists">
-        <a
-          className="list-link"
-          target="_blank"
-          href="https://bit.ly/wholesome-donate-mall">
-          <img
-            className="link-img"
-            src="https://user-images.githubusercontent.com/35059428/73065048-92cfae00-3edd-11ea-900d-9be5316e5e5e.png"
-          />
-          <h1>후원몰</h1>
-        </a>
-        <a
-          className="list-link"
-          target="_blank"
-          href="https://www.youtube.com/channel/UC4kb6gxOTKVBIKcFfZPiwoQ?view_as=subscriber">
-          <img
-            className="link-img"
-            src="https://user-images.githubusercontent.com/35059428/73051291-60ae5400-3ebd-11ea-8581-734032706c06.png"
-          />
-        </a>
-        <a
-          className="list-link"
-          target="_blank"
-          href="https://instagram.com/6catmom.zena">
-          <img
-            className="link-img"
-            src=" https://scontent-sin6-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/66765412_573054289894341_4324031579364691792_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=109&_nc_ohc=MiMmX_JlqTYAX86e8MC&oh=2b64f3c204382a120ea1b6317b23fd74&oe=5ED72C9C "
-          />
-          <div>
-            <h1>캣맘 제나</h1>
-            <p>@6catmom.zena</p>
-          </div>
-        </a>
-        <a
-          className="list-link"
-          target="_blank"
-          href="https://instagram.com/___sj___y">
-          <img
-            className="link-img"
-            src="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-15/e35/47581801_323181528283940_4634403341321352258_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=108&_nc_ohc=qw80QeZfxH0AX9pz6Q4&oh=60674f16f04990cef52b37c64ec03f66&oe=5EC212C0"
-          />
-          <div>
-            <h1>캣대디 SJ</h1>
-            <p>@___sj___y</p>
-          </div>
-        </a>
+        <Popover
+          placement="right"
+          content={
+            <div>
+              <h3>
+                홀썸이네
+                <br />
+                후원하기
+              </h3>
+            </div>
+          }>
+          <a
+            className="list-link"
+            target="_blank"
+            href="https://bit.ly/wholesome-donate-mall">
+            <img
+              style={{padding: '1vw'}}
+              className="link-img"
+              src="https://user-images.githubusercontent.com/35059428/73065048-92cfae00-3edd-11ea-900d-9be5316e5e5e.png"
+            />
+            <div>
+              <h1>후원몰</h1>
+              <p>조금 더 나은 방식의 후원</p>
+            </div>
+          </a>
+        </Popover>
+        <Popover
+          placement="right"
+          content={
+            <div>
+              <h3>
+                홀썸이네 <br /> 일상 훔쳐보기
+              </h3>
+            </div>
+          }>
+          <a
+            className="list-link"
+            target="_blank"
+            href="https://www.youtube.com/channel/UC4kb6gxOTKVBIKcFfZPiwoQ?view_as=subscriber">
+            <img
+              style={{padding: '1vw'}}
+              className="link-img"
+              src="https://user-images.githubusercontent.com/35059428/73051291-60ae5400-3ebd-11ea-8581-734032706c06.png"
+            />
+            <h1>유튜브 구독</h1>
+          </a>
+        </Popover>
+        <Popover
+          placement="left"
+          content={
+            <div>
+              <h3>
+                캣맘과 <br />
+                소통하러가기
+              </h3>
+            </div>
+          }>
+          <a
+            className="list-link"
+            target="_blank"
+            href="https://instagram.com/6catmom.zena">
+            <img
+              className="link-img"
+              src=" https://scontent-sin6-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/66765412_573054289894341_4324031579364691792_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=109&_nc_ohc=MiMmX_JlqTYAX86e8MC&oh=2b64f3c204382a120ea1b6317b23fd74&oe=5ED72C9C "
+            />
+            <div>
+              <h1>캣맘 제나</h1>
+              <p>@6catmom.zena</p>
+            </div>
+          </a>
+        </Popover>
+        <Popover
+          placement="left"
+          content={
+            <div>
+              <h3>
+                캣대디와
+                <br /> 소통하러가기
+              </h3>
+            </div>
+          }>
+          <a
+            className="list-link"
+            target="_blank"
+            href="https://instagram.com/___sj___y">
+            <img
+              className="link-img"
+              src="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-15/e35/47581801_323181528283940_4634403341321352258_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=108&_nc_ohc=qw80QeZfxH0AX9pz6Q4&oh=60674f16f04990cef52b37c64ec03f66&oe=5EC212C0"
+            />
+            <div>
+              <h1>캣대디 SJ</h1>
+              <p>@___sj___y</p>
+            </div>
+          </a>
+        </Popover>
       </Layout>
     </>
   );
